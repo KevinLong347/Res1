122575f05af8b6677238e48b376a35619cfcb0b9
KevinLong347 <kevinlong2105@gmail.com>
2024-10-05 19:12:45 +0700

    Initial Commit

---
+++ Bài 1 - HP2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bài 1 - HP2
+//
+//  Created by Kevin on 5/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
